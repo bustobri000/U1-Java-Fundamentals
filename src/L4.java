aa6dd8e1327e62d298b4a876647a495a94402e77
bustobri000 <bustobri000@warren.k12.in.us>
2021-08-09 12:59:10 -0400

    1.3 - 1.5 Notes

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,20 @@
 public class L4 {
+    public static void main(String[] args) {
+     int value = 5;
+
+    //Declare a variable - stating the datatype and identifier
+    double total;
+    String message = "Hi";
+    final double GRAVITY = -9.81;
+
+    //Assign a value
+    total = 6;
+    System.out.println(message + ", the value is " + total);
+
+    total = 10;
+    message = "bye";
+    //GRAVITY = 10;  -- this is bad
+
+    System.out.println(message + ", the value is " + total);
+    }
 }
